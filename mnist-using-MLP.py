6b76b878115125a5f9446401cc23866250d6cf8c
Ebun Egbewunmi <ebunegbewunmi@gmail.com>
2024-10-16 15:49:43 -0500

    evaluate model

--- mnist-using-MLP.py
+++ mnist-using-MLP.py
@@ -44,3 +44,31 @@ mlp = MLPClassifier(
 
 # Train the MLPClassifier
 mlp.fit(X_train, y_train)
+
+# Show the accuracy on the training and test sets
+
+print(f"Training set score: {mlp.score(X_train, y_train)}")
+print(f"Test set score: {mlp.score(X_test, y_test)}")
+
+# Show the images, predictions, and original labels for 10 images
+
+# Get the predictions for the test dataset
+predictions = mlp.predict(X_test)
+
+# Show the predictions in a grid
+plt.figure(figsize=(8, 4))
+
+for index, (image, prediction, label) in enumerate(
+    zip(X_test[0:10], predictions[0:10], y_test[0:10])
+):
+    plt.subplot(2, 5, index + 1)
+    plt.imshow(np.reshape(image, (28, 28)), cmap=plt.cm.gray)
+
+    # Green if correct, red if incorrect
+    fontcolor = "g" if prediction == label else "r"
+    plt.title(
+        "Prediction: %i\n Label: %i" % (prediction, label), fontsize=10, color=fontcolor
+    )
+
+    plt.axis("off")  # hide axes
+    plt.show()
